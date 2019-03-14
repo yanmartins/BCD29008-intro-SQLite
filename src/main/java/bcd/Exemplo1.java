5fec4df7e65a914572c6f7f55090e4994710ffc0
Yan Lucas Martins <yan.l.m.1998@gmail.com>
2019-03-14 16:42:08 -0300

    Iniciando SQLite

--- src/main/java/bcd/Exemplo1.java
+++ src/main/java/bcd/Exemplo1.java
@@ -1,5 +1,28 @@
 package bcd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Exemplo1 {
 
+    private static final String dbPath = "src/main/resources/lab01.sqlite";
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        System.out.println("Olá mundo");
+
+        Class.forName("org.sqlite.JDBC"); // Uma camada de abstração do Java para comunicação com o Banco
+
+        Connection conexao = DriverManager.getConnection("jdbc:sqlite:" + dbPath); // Criando uma conexão
+
+        Statement stmt = conexao.createStatement();
+
+        String query = "INSERT INTO aluno VALUES (2,'juca',80,180,'juca@email.com')";
+
+        stmt.executeUpdate(query);
+        stmt.close();
+        conexao.close();
+
+    }
 }
